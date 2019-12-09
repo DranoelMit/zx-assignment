47c1b698b2121cbe299d69ea6efb5aa7eb5c72cb
DranoelMit <tim.matthew.leonard@gmail.com>
2019-12-09 01:57:35 -0500

    build static site

---
+++ build/precache-manifest.b045a45ad0544d8baa4a21ca99a8813b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d57838563736dca9047386b4c7c4fc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed90b40951caab39de2",
+    "url": "/static/css/main.8811b78c.chunk.css"
+  },
+  {
+    "revision": "a9a4382c1091df84cf07",
+    "url": "/static/js/2.446119ea.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.446119ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ed90b40951caab39de2",
+    "url": "/static/js/main.efabd80c.chunk.js"
+  },
+  {
+    "revision": "8f16f797bd988fcc60f7",
+    "url": "/static/js/runtime-main.0a8a01dc.js"
+  }
+]);
