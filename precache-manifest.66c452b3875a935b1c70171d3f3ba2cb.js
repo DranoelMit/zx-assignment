890835765dccd24bf52dd86bf03bd1ad3fa9c496
DranoelMit <tim.matthew.leonard@gmail.com>
2019-12-09 02:09:57 -0500

    Updates

---
+++ precache-manifest.66c452b3875a935b1c70171d3f3ba2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2132ce9485f16d0cfc3ba3fb2c55e74a",
+    "url": "/zx-assignment/index.html"
+  },
+  {
+    "revision": "3fcad2c8da3b7bf97916",
+    "url": "/zx-assignment/static/css/main.8811b78c.chunk.css"
+  },
+  {
+    "revision": "a9a4382c1091df84cf07",
+    "url": "/zx-assignment/static/js/2.446119ea.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/zx-assignment/static/js/2.446119ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fcad2c8da3b7bf97916",
+    "url": "/zx-assignment/static/js/main.6c0524c0.chunk.js"
+  },
+  {
+    "revision": "dc20adbcc1cd482b96f9",
+    "url": "/zx-assignment/static/js/runtime-main.f6e54e5b.js"
+  }
+]);
